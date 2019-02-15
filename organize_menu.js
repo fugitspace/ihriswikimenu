2e6b6519ac7b3cdc6f51b9119206c87c83bfe58d
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-02-14 23:26:07 -0500

    further refactoring

--- organize_menu.js
+++ organize_menu.js
@@ -12,10 +12,12 @@ const rl = readline.createInterface({
     input: fd,
     crlfDelay: Infinity // wait for both \r and \n
 });
+// organize the line
 rl.on('line', (line) => {
     organize(line);
 });
 
+//when done reading the urls into the object, we write to file as JSON
 fd.on('close', () => {
     const ws = fs.createWriteStream('data/organized_links.json', {flags: 'w'});
     ws.write(JSON.stringify(menu));
@@ -25,28 +27,12 @@ fd.on('close', () => {
 function organize(line) {
     // parse line into json object
     // {"category":"Blueprints","url":"/wiki/Organizational_Charts","text":"Organizational Charts"}
-    const line_json = JSON.parse(line);
-    // read the key
-    if(keys.includes(line_json.category)) {
-        addToMenu(line_json, true);
-    } else {
-        addToMenu(line_json, false);
-    }
-}
-
-function addToMenu(item, processed) {
-    if(!processed) {
-        keys.push(item.category);
-    }
+    const item = JSON.parse(line);
     if(!menu[item.category]){
-        menu[item.category] = [{ 
-            url: item.url, 
-            text: item.text
-        }];
-    } else {
-        menu[item.category].push({ 
-            url: item.url, 
-            text: item.text || ''
-        });
+        menu[item.category] = [];
     }
+    menu[item.category].push({ 
+        url: item.url, 
+        text: item.text || ''
+    });
 }
