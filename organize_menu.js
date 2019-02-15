1b73546604cd53fe0ee9babe1804ca91897a8df0
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-02-14 22:01:16 -0500

    add node realine interface to read the file

--- organize_menu.js
+++ organize_menu.js
@@ -5,6 +5,16 @@ const fd = new events.EventEmitter();
 
 var menu = {};
 var keys = [];
+
+// read the file line by line and process
+const rl = readline.createInterface({
+    input: fs.createReadStream('data/categories.txt'),
+    crlfDelay: Infinity // wait for both \r and \n
+});
+rl.on('line', (line) => {
+    organize(line);
+});
+
 function organize(line) {
     // parse line into json object
     // {"category":"Blueprints","url":"/wiki/Organizational_Charts","text":"Organizational Charts"}
