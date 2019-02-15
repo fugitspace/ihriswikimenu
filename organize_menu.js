1af8aa194bf14d8385ac45c428220e271e7e22b1
Sovello Hildebrand <sovellohpmgani@gmail.com>
2019-02-14 22:19:14 -0500

    all done, find a way to check when readline is done and then write the output to a file

--- organize_menu.js
+++ organize_menu.js
@@ -8,17 +8,19 @@ var keys = [];
 
 // read the file line by line and process
 const rl = readline.createInterface({
-    input: fs.createReadStream('data/categories.txt'),
+    input: fs.createReadStream('data/all_urls.txt'),
     crlfDelay: Infinity // wait for both \r and \n
 });
 rl.on('line', (line) => {
     organize(line);
 });
+rl.on('end', () => {
+    console.log(menu);
+})
 
 function organize(line) {
     // parse line into json object
     // {"category":"Blueprints","url":"/wiki/Organizational_Charts","text":"Organizational Charts"}
-
     const line_json = JSON.parse(line);
     // read the key
     if(keys.includes(line_json.category)) {
@@ -32,8 +34,16 @@ function addToMenu(item, processed) {
     if(!processed) {
         keys.push(item.category);
     }
-    menu[item.category].push({ 
-        url: item.url, 
-        text: item.text
-    });
+    if(!menu[item.category]){
+        menu[item.category] = [{ 
+            url: item.url, 
+            text: item.text
+        }];
+    } else {
+        menu[item.category].push({ 
+            url: item.url, 
+            text: item.text || ''
+        });
+    }
+    console.log(menu);
 }
